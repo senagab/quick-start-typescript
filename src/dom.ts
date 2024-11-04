3f50bd4741131e9a94482d2d9daacc2ca10192e8
senagab <senagab@gmail.com>
2024-11-04 10:08:20 -0400

    feat: criação de nova branch + chore: remoção de codigo aula

--- src/dom.ts
+++
@@ -1,8 +0,0 @@
-const campoNome = document.getElementById('nome') ;
-const formulario = document.getElementsByTagName('form');
-const botaoEnviar = document.getElementById('btn-enviar');
-
-// ? só vai enviar 
-botaoEnviar?.addEventListener('click', (e: MouseEvent) {
-    e.pageY;
-})
--- src/teste.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-let nome = "gian";
-nome = "27";
