67a266cacf2145ea95e554dd5de9560dfbce91cc
senagab <senagab@gmail.com>
2024-11-04 10:02:33 -0400

    feat: ts para manipulação de DOM

---
+++ src/dom.js
@@ -0,0 +1,8 @@
+"use strict";
+const campoNome = document.getElementById('nome');
+const formulario = document.getElementsByTagName('form');
+const botaoEnviar = document.getElementById('btn-enviar');
+// ? só vai enviar 
+botaoEnviar === null || botaoEnviar === void 0 ? void 0 : botaoEnviar.addEventListener('click', (e) => {
+    e.pageY;
+});
---
+++ src/dom.ts
@@ -0,0 +1,8 @@
+const campoNome = document.getElementById('nome') ;
+const formulario = document.getElementsByTagName('form');
+const botaoEnviar = document.getElementById('btn-enviar');
+
+// ? só vai enviar 
+botaoEnviar?.addEventListener('click', (e: MouseEvent) {
+    e.pageY;
+})
